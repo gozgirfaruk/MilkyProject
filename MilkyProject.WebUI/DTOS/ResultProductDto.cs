4b61e683efadb48c33ccfd9be2b43aea8bdcf38f
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-04-27 17:22:34 +0300

    add view for product

---
+++ MilkyProject.WebUI/DTOS/ResultProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MilkyProject.WebUI.DTOS
+{
+    public class ResultProductDto
+    {
+        public int productID { get; set; }
+        public string name { get; set; }
+        public float oldPrice { get; set; }
+        public float newPrice { get; set; }
+        public string imageUrl { get; set; }
+        public bool status { get; set; }
+    }
+}
+
+
